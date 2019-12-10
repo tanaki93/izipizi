c323cb612943e7e21f06e575f56ea5a487fe21a7
tanaki93 <tanaki.93@gmail.com>
2019-12-10 23:01:22 +0600

    added title data

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -1,8 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from product_app.models import Document
+
 
 class UserSerializer(serializers.ModelSerializer):
+    is_related = serializers.SerializerMethodField()
+
     class Meta:
         model = get_user_model()
-        fields = 'id first_name last_name username user_type phone email'.split()
+        fields = 'id first_name last_name username user_type phone email is_related'.split()
+
+    def get_is_related(self, obj):
+        count = Document.objects.filter(user=obj).count()
+        if count > 0:
+            return True
+        return False
