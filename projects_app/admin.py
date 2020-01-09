e0c151bed33f7d3249bb7bec21d03a952a7cbf73
tanaki93 <tanaki.93@gmail.com>
2020-01-10 02:22:41 +0600

    added title data

--- projects_app/admin.py
+++ projects_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from projects_app.models import Order, OrderItem, OrderItemComment, OrderPackage, CommentImage, OrderPacket, \
-    PacketProduct
+    PacketProduct, Flight
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
@@ -9,6 +9,7 @@ admin.site.register(OrderPackage)
 admin.site.register(OrderItemComment)
 admin.site.register(CommentImage)
 admin.site.register(OrderPacket)
+admin.site.register(Flight)
 admin.site.register(PacketProduct)
 
 
